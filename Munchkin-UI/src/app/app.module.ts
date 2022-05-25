84dc6a8f7496cf9ed9f2ee525e781cd0a8b687b6
Vladyslav Maryniak <vladyslav.maryniak@gmail.com>
2022-05-25 22:25:04 +0300

    Implement game component

--- Munchkin-UI/src/app/app.module.ts
+++ Munchkin-UI/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
 import { GameLobbyComponent } from './pages/game-lobby/game-lobby.component';
+import { GameComponent } from './pages/game/game.component';
 import { ActionButtonComponent } from './components/action-button/action-button.component';
 import { ActionControlAreaDirective } from './directives/action-control-area.directive';
 import { MetadataDialogComponent } from './components/metadata-dialog/metadata-dialog.component';
@@ -39,6 +40,11 @@ const appRoutes: Routes = [
     canActivate: [AuthenticationGuard],
   },
   {
+    path: 'game/:game-id',
+    component: GameComponent,
+    canActivate: [AuthenticationGuard],
+  },
+  {
     path: 'game/:game-id/lobby',
     component: GameLobbyComponent,
     canActivate: [AuthenticationGuard],
@@ -53,6 +59,7 @@ const appRoutes: Routes = [
     SignInComponent,
     SignUpComponent,
     GameLobbyComponent,
+    GameComponent,
     ActionButtonComponent,
     ActionControlAreaDirective,
     MetadataDialogComponent,
