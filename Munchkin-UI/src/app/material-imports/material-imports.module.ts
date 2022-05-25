1af86cb51442b962e6999b3f0b2269e305622654
Vladyslav Maryniak <vladyslav.maryniak@gmail.com>
2022-05-25 22:10:46 +0300

    Extend Material imports

--- Munchkin-UI/src/app/material-imports/material-imports.module.ts
+++ Munchkin-UI/src/app/material-imports/material-imports.module.ts
@@ -6,6 +6,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [],
@@ -16,6 +22,12 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatChipsModule,
     MatToolbarModule,
     MatCheckboxModule,
+    MatListModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatTooltipModule,
+    MatSnackBarModule,
+    MatDialogModule,
   ],
   exports: [
     MatButtonModule,
@@ -23,6 +35,12 @@ import { MatCheckboxModule } from '@angular/material/checkbox';
     MatChipsModule,
     MatToolbarModule,
     MatCheckboxModule,
+    MatListModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatTooltipModule,
+    MatSnackBarModule,
+    MatDialogModule,
   ],
 })
 export class MaterialImportsModule {}
