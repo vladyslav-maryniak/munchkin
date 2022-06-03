3a56b043e30b927b3f05b6a5851627a11c31d28c
Vladyslav Maryniak <vladyslav.maryniak@gmail.com>
2022-06-03 13:34:34 +0300

    Add Minio image service

---
+++ Munchkin.API/Controllers/ImagesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Munchkin.Application.Services.Base;
+
+namespace Munchkin.API.Controllers
+{
+    [ApiController]
+    [Route("api/images")]
+    public class ImagesController : ControllerBase
+    {
+        private readonly IImageService imageService;
+
+        public ImagesController(IImageService imageService)
+        {
+            this.imageService = imageService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetImageAsync(
+            string id, CancellationToken cancellationToken)
+        {
+            var reply = await imageService.GetImageAsync(id, cancellationToken);
+
+            if (reply is null || reply.Data is null || reply.ObjectStat is null)
+            {
+                return NotFound();
+            }
+
+            return File(reply.Data, reply.ObjectStat.ContentType);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> UploadImagesAsync(
+            [FromForm] IEnumerable<IFormFile> files, CancellationToken cancellationToken)
+        {
+            await imageService.UploadImagesAsync(files, cancellationToken);
+
+            return Ok();
+        }
+    }
+}
---
+++ Munchkin.Shared/Options/MinioOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Munchkin.Shared.Options
+{
+    public class MinioOptions
+    {
+        public string? Endpoint { get; set; }
+        public string? AccessKey { get; set; }
+        public string? SecretKey { get; set; }
+        public string? BucketName { get; set; }
+    }
+}
