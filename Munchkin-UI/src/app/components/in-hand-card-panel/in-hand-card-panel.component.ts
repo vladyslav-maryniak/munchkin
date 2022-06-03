ed5b4b19a5e9bcfe965907eae101370245ddb734
Vladyslav Maryniak <vladyslav.maryniak@gmail.com>
2022-06-03 13:44:42 +0300

    Implement card selection with options

--- Munchkin-UI/src/app/components/in-hand-card-panel/in-hand-card-panel.component.ts
+++ Munchkin-UI/src/app/components/in-hand-card-panel/in-hand-card-panel.component.ts
@@ -1,10 +1,14 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
-import { lastValueFrom } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+import { lastValueFrom, Subscription } from 'rxjs';
 import { Game } from 'src/app/models/game';
 import { MunchkinCard } from 'src/app/models/munchkin-card';
 import { Player } from 'src/app/models/player';
+import { CardService } from 'src/app/services/card.service';
 import { GameService } from 'src/app/services/game.service';
+import { SharedDataService } from 'src/app/services/shared-data.service';
+import { SignalrService } from 'src/app/services/signalr.service';
 import { MetadataDialogComponent } from '../metadata-dialog/metadata-dialog.component';
 
 @Component({
@@ -12,18 +16,65 @@ import { MetadataDialogComponent } from '../metadata-dialog/metadata-dialog.comp
   templateUrl: './in-hand-card-panel.component.html',
   styleUrls: ['./in-hand-card-panel.component.css'],
 })
-export class InHandCardPanelComponent {
-  @Input() game!: Game;
-  @Input() player!: Player;
+export class InHandCardPanelComponent implements OnInit, OnDestroy {
+  game!: Game;
+  player!: Player;
+
+  private subscriptions: Subscription[] = [];
+
+  multiselect: boolean = false;
+  selectedCards: MunchkinCard[] = [];
+
+  get totalGoldPieces(): number {
+    return this.selectedCards
+      .map((x) => x.goldPieces ?? 0)
+      ?.reduce((total, x) => total + x);
+  }
 
   get inHandCards(): MunchkinCard[] {
-    const place = this.game.table.places.find(
+    const place = this.game?.table.places.find(
       (x) => x.player.id === this.player.id
     );
     return place?.inHandCards ?? [];
   }
 
-  constructor(private gameService: GameService, public dialog: MatDialog) {}
+  private eventHandlers = new Map<string, (...args: any[]) => Promise<void>>([
+    ['ItemCardPlayedEvent', this.onItemCardPlayedEvent],
+    ['OneShotCardPlayedEvent', this.onOneShotCardPlayedEvent],
+    ['GoUpLevelCardPlayedEvent', this.onGoUpLevelCardPlayedEvent],
+    ['PlayerSoldCardsEvent', this.onPlayerSoldCardsEvent],
+  ]);
+
+  constructor(
+    private route: ActivatedRoute,
+    private gameService: GameService,
+    private cardService: CardService,
+    private signalrService: SignalrService,
+    private dataService: SharedDataService,
+    public dialog: MatDialog
+  ) {}
+
+  async ngOnInit(): Promise<void> {
+    const gameId = this.route.snapshot.paramMap.get('game-id') ?? '';
+    const game = await this.dataService.getGame(gameId);
+    this.subscriptions.push(game.subscribe((x: Game) => (this.game = x)));
+
+    this.subscriptions.push(
+      this.signalrService.gameEvents.subscribe(this.onEvent)
+    );
+
+    this.subscriptions.push(
+      this.dataService.getPlayer().subscribe((x: Player) => (this.player = x))
+    );
+  }
+
+  private onEvent = async (event: string): Promise<void> => {
+    if (this.eventHandlers.has(event)) {
+      await this.eventHandlers.get(event)?.call(this);
+      this.game = await this.gameService.getGame(this.game.id);
+      this.dataService.setGame(this.game);
+    }
+  };
 
   async playCard(card: MunchkinCard): Promise<void> {
     let metadata = card.metadata;
@@ -50,17 +101,74 @@ export class InHandCardPanelComponent {
     );
   }
 
-  getCardDescription(c: MunchkinCard): string {
-    const description: string[] = [];
-    if (c.description) {
-      description.push(c.description);
+  async sellCards(): Promise<void> {
+    const cardIds = this.selectedCards.map((x) => x.id);
+    this.clearSelectedCards();
+    await this.gameService.sellCards(this.game.id, this.player.id, cardIds);
+  }
+
+  async onItemCardPlayedEvent(): Promise<void> {}
+
+  async onOneShotCardPlayedEvent(): Promise<void> {}
+
+  async onGoUpLevelCardPlayedEvent(): Promise<void> {}
+
+  async onPlayerSoldCardsEvent(): Promise<void> {}
+
+  onClick(card: MunchkinCard): void {
+    this.isCardSelected(card) ? this.deselectCard(card) : this.selectCard(card);
+  }
+
+  isCardSelected(card: MunchkinCard): boolean {
+    return this.selectedCards.includes(card);
+  }
+
+  selectCard(card: MunchkinCard): void {
+    if (
+      (!this.cardService.isItemCard(card) && this.multiselect) ||
+      (this.cardService.isItemCard(card) && this.isPlayerInCombat())
+    ) {
+      return;
     }
-    if (c.badStuff) {
-      description.push(`Bad stuff: ${c.badStuff}`);
+
+    if (this.multiselect === false) {
+      this.clearSelectedCards();
     }
-    if (c.goldPieces) {
-      description.push(`${c.goldPieces} Gold Pieces`);
+    this.selectedCards.push(card);
+  }
+
+  deselectCard(card: MunchkinCard): void {
+    if (this.selectedCards.length === 1) {
+      this.multiselect = false;
     }
-    return description.join('\n');
+    const index = this.selectedCards.indexOf(card);
+    this.selectedCards.splice(index, 1);
+  }
+
+  clearSelectedCards(): void {
+    this.multiselect = false;
+    this.selectedCards = [];
+  }
+
+  switchSelectMode(): void {
+    if (!this.isPlayerInCombat()) {
+      this.multiselect = !this.multiselect;
+    }
+  }
+
+  isPlayerInCombat(): boolean {
+    return this.gameService.isPlayerInCombat(this.game, this.player);
+  }
+
+  getCardDescription(card: MunchkinCard): string {
+    return this.cardService.getCardDescription(card);
+  }
+
+  getCardImageSrc(card: MunchkinCard): string {
+    return this.cardService.getCardImageSrc(card);
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((x) => x.unsubscribe());
   }
 }
