449bdcfd1d385e8ca08a2654bd3aae5b14fbaa59
vladyslav-maryniak <vladyslav.maryniak@gmail.com>
2022-06-30 06:57:34 +0300

    Implement validation of Player requests

--- Munchkin.Domain/Queries/GetPlayer.cs
+++ Munchkin.Domain/Queries/GetPlayer.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Munchkin.Application.Services.Base;
+using Munchkin.Domain.Validation;
 using Munchkin.Shared.Models;
 
 namespace Munchkin.Domain.Queries
@@ -7,6 +8,22 @@ namespace Munchkin.Domain.Queries
     public static class GetPlayer
     {
         public record Query(Guid PlayerId) : IRequest<Response>;
+
+        public class Validator : IValidationHandler<Query>
+        {
+            private readonly IGameRepository repository;
+
+            public Validator(IGameRepository repository)
+            {
+                this.repository = repository;
+            }
+
+            public async Task<ValidationResult> Validate(Query request)
+            {
+                var player = await repository.GetPlayerAsync(request.PlayerId);
+                return player is null ? ValidationError.InvalidPlayerId : ValidationResult.Success;
+            }
+        }
 
         public class Handler : IRequestHandler<Query, Response>
         {
@@ -20,10 +37,14 @@ namespace Munchkin.Domain.Queries
             public async Task<Response> Handle(Query request, CancellationToken cancellationToken)
             {
                 var player = await repository.GetPlayerAsync(request.PlayerId, cancellationToken);
-                return new Response(player);
+
+                return new Response() { Player = player };
             }
         }
 
-        public record Response(Player Player);
+        public record Response : CqrsResponse
+        {
+            public Player? Player { get; set; }
+        }
     }
 }
